73fa15a4bfa6313bd9b2f0492d9cb78800310e01
Ravi <ravikaran90@yahoo.com>
2025-07-30 23:56:46 -0700

    Valid Array Splits

---
+++ array_split.py
@@ -0,0 +1,32 @@
+class Solution: 
+    def prefix_sum(self,nums):
+        valid_splits=0
+        prefix=[nums[0]]
+        for i in range(1,len(nums)):
+            prefix.append(nums[i]+prefix[len(prefix)-1])
+        #return prefix
+        #[10, 14,  6, 13]
+        for i in range(0,len(prefix)):
+            left=prefix[i]
+            right=prefix[-1]-prefix[i]
+            if left>=right:
+                valid_splits+=1
+            #sum=prefix[i]-prefix[j]+nums[i]
+            #if sum>nums:
+        return valid_splits
+        #First Section has a sum greater than or equal 
+        #to the sum of the second section
+
+def main():
+    nums=[2,3,1,0]
+    #nums=[10,  4, -8,  7]
+    #ps-=[10, 14,  6, 13]
+    #.   [10][4,-8,7] | [10,4][-8,7] | [10,4,-8][7]
+
+    #[10][10,4,-8][10,4,-8,7]
+    obj=Solution()
+    res=obj.prefix_sum(nums)
+    print("Result:",res)
+
+if __name__=="__main__":
+    main()
