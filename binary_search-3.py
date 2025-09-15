46764605f29bf25eff040cf7500ac0fb1f0be488
Ravi <ravikaran90@yahoo.com>
2025-09-15 15:56:43 -0700

    Binary Search

--- binary_search-3.py
+++ binary_search-3.py
@@ -3,18 +3,19 @@ class Solution:
         left=0
         right=len(nums)-1
         while left<=right:
-            mid=(left+right)//2
+            mid=(right+left)//2
             if nums[mid]==target:
                 return mid
             elif target<nums[mid]:
                 right=mid-1
             elif target>nums[mid]:
                 left=mid+1
+        return False
 
 def main():
-    nums=[100,134,146,165,190,250,340,560,600,780,920]
+    nums=[10,30,60,80,100,120,250,500,750,800]
     obj=Solution()
-    res=obj.binary_search(nums,560)
+    res=obj.binary_search(nums,500)
     print("Result:",res)
 
 if __name__=="__main__":
