bf74c8e56102e4231ae2e3e0425513e7f2869f09
Ravi <ravikaran90@yahoo.com>
2025-09-25 20:42:59 -0700

    Longest Common Prefix

---
+++ lcp-6.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lcp(self,strs):
+        if len(strs)==0:
+            return ""
+        prefix=strs[0]
+        for i in range(1,len(strs)):
+            while strs[i].find(prefix)!=0:
+                prefix=prefix[0:len(prefix)-1]
+                if len(prefix)==0:
+                    return ""
+        return prefix
+
+def main():
+    strs=["ravi","ravikaran","ravikarananand"]
+    obj=Solution()
+    res=obj.lcp(strs)
+    print("Result:",res)
+
+if __name__=="__main__":
+    main()
