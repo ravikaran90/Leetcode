7640ebeb6d08051575993f2e0d42d432119846cd
Ravi <ravikaran90@yahoo.com>
2025-10-05 00:31:16 -0700

    Valid Parentheses

---
+++ valid_parentheses-7.py
@@ -0,0 +1,23 @@
+class Solution:
+    def valid_parentheses(self,string):
+        diction={"(":")","{":"}","[":"]"}
+        stack=[]
+        for c in string:
+            if c in "({[":
+                stack.append(c)
+            elif len(stack)==0:
+                return False
+            elif diction[stack.pop()]!=c:
+                return False
+                #elif 
+                #return False
+        return len(stack)==0
+
+def main():
+    string="({[})"
+    obj=Solution()
+    res=obj.valid_parentheses(string)
+    print("Result:",res)
+
+if __name__=="__main__":
+    main()
