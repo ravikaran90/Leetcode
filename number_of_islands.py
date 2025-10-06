974899c4b77191c9bac87bdabcdd7deba3d2aa52
Ravi <ravikaran90@yahoo.com>
2025-10-05 23:46:23 -0700

    Number of Islands

---
+++ number_of_islands.py
@@ -0,0 +1,43 @@
+import collections 
+
+class Solution:
+    def number_of_islands(self,grid):
+        if not grid:
+            return 0
+        rows,cols=len(grid),len(grid[0])
+        visit=set()
+        islands=0
+        
+        def bfs(r,c):
+            q=collections.deque()
+            visit.add((r,c))
+            q.append((r,c))
+            while q:
+                row,col=q.popleft()
+                directions=[[1,0],[-1,0],[0,1],[0,-1]]
+                for dr,dc in directions:
+                    r,c=row+dr,col+dc
+                    if (r in range(rows) and c in range(cols) and grid[r][c]=="1" and (r,c) not in visit):
+                        q.append((r,c))
+                        visit.add((r,c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]=="1" and (r,c) not in visit:
+                    bfs(r,c)
+                    islands+=1
+        return islands
+
+def main():
+    grid = [
+    ["1","1","1","1","0"],
+    ["1","1","0","1","0"],
+    ["1","1","0","0","0"],
+    ["0","0","0","0","0"]
+    ]
+    obj=Solution()
+    res=obj.number_of_islands(grid)
+    print("Result:",res)
+
+if __name__=="__main__":
+    main()
