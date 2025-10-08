ba55a8ec05542d4df20f907825468533b0293a39
Ravi <ravikaran90@yahoo.com>
2025-10-08 00:41:08 -0700

    Closest Palindrome

---
+++ closest_palindrome.py
@@ -0,0 +1,57 @@
+class Solution:
+    def closest_palindrome(self,n):
+        len_n=len(n)
+        is_even=len_n%2==0
+        mid=len_n//2-1 if len_n%2==0 else len_n//2
+        first_half=int(n[:mid+1])
+        possible_solutions=[]
+
+        possible_solutions.append(
+            self.half_to_palindrome(first_half,is_even)
+        )
+        possible_solutions.append(
+            self.half_to_palindrome(first_half-1,is_even)
+        )
+        possible_solutions.append(
+            self.half_to_palindrome(first_half+1,is_even)
+        )
+        possible_solutions.append(
+            10**(len_n-1)-1
+        )
+        possible_solutions.append(
+            10**len(n)+1
+        )
+
+        diff=float("inf")
+        res=0
+        int_n=int(n)
+
+        for candidate in possible_solutions:
+            if candidate==int_n:
+                continue
+            elif abs(candidate-int_n)<diff:
+                diff=abs(candidate-int_n)
+                res=candidate
+            elif abs(candidate-int_n)==diff:
+                res=min(candidate,res)
+    
+        return str(res)
+    
+    def half_to_palindrome(self,num,is_even):
+        res=num
+        if not is_even:
+            num//=10
+        while num>0:
+            res=res*10+num%10
+            num//=10
+        return res
+
+
+def main():
+    n="225"
+    obj=Solution()
+    res=obj.closest_palindrome(n)
+    print("Result:",res)
+
+if __name__=="__main__":
+    main()
