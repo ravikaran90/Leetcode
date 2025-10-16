4e63f1e807d037b79d8a60647c1a579e400d56c2
Ravi <ravikaran90@yahoo.com>
2025-10-15 23:30:07 -0700

    Binary Search

--- binary_search-3.py
+++ binary_search-3.py
@@ -1,4 +1,4 @@
-class Solution: 
+class Solution:
     def binary_search(self,nums,target):
         left=0
         right=len(nums)-1
@@ -6,17 +6,18 @@ class Solution:
             mid=(right+left)//2
             if nums[mid]==target:
                 return mid
-            elif target<nums[mid]:
-                right=mid-1
             elif target>nums[mid]:
                 left=mid+1
-        return False
+            elif target<nums[mid]:
+                right=mid-1
+        return -1
 
 def main():
-    nums=[10,30,60,80,100,120,250,500,750,800]
+    nums=[7,8,12,15,18,20,22,25,30,43,57,72]
+    target=57
     obj=Solution()
-    res=obj.binary_search(nums,500)
-    print("Result:",res)
+    res=obj.binary_search(nums,target)
+    print("Number at Position:",res)
 
 if __name__=="__main__":
     main()
