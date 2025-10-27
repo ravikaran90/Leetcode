6c823abc8fc9ae9d2bd0d6df5a420f284ec39b16
Ravi <ravikaran90@yahoo.com>
2025-10-26 20:13:20 -0700

    Longest Substring without Repeat

---
+++ longest_substring_without_repeat-3.py
@@ -0,0 +1,29 @@
+from collections import Counter
+
+class Solution:
+    def longest_substring_without_repeat(self,s):
+        chars=Counter()
+        left=0
+        right=0
+        res=0
+
+        while right<len(s):
+            r=s[right]
+            chars[r]+=1
+
+            while chars[r]>1:
+                l=s[left]
+                chars[l]-=1
+                left+=1
+            res=max(res,right-left+1)
+            right+=1
+        return res
+
+def main():
+    s="eceba"
+    obj=Solution()
+    res=obj.longest_substring_without_repeat(s)
+    print("Result:",res)
+
+if __name__=="__main__":
+    main()
