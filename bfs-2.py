32e95ca0a18230589a34ed8a354ec5009fc70d52
Ravi <ravikaran90@yahoo.com>
2025-11-04 23:58:50 -0800

    Breadth First Search

---
+++ bfs-2.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init(self,val,left=None,right=None):
+        self.val=val
+        self.left=left
+        self.right=right
+from collections import deque
+
+class Solution:
+    def bfs():
+        q=deque([root])
+
+def main():
+    root=Node(50)
+    obj=Solution()
+    res=obj.bfs(root)
+    print("Result:",res)
+
+if __name__=="__main__":
+    main()
