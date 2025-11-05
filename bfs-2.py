aa5896e746e34be1deb26b9c63dbbe9e30ccee90
Ravi <ravikaran90@yahoo.com>
2025-11-05 00:29:59 -0800

    Breadth First Seach

--- bfs-2.py
+++ bfs-2.py
@@ -7,12 +7,30 @@ class Node:
         self.right=right
 
 class Solution:
-    def bfs():
+    def bfs(self,root):
+        result=[]
         q=deque([root])
-        
+
+        while q:
+            level=[]
+            for i in range(len(q)):
+                node=q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                result.append(level)
+        return result
 
 def main():
     root=Node(50)
+    root.left=Node(25)
+    root.left.left=Node(10)
+    root.left.right=Node(30)
+    root.right=Node(80)
+    root.right.left=Node(75)
+    root.right.right=Node(100)
     obj=Solution()
     res=obj.bfs(root)
     print("Result:",res)
