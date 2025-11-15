aa170984262a96c2b1052d91f75dfca26a687da2
Ravi <ravikaran90@yahoo.com>
2025-11-14 23:59:05 -0800

    Two Sum

---
+++ two_sum-8.py
@@ -0,0 +1,12 @@
+class Solution:
+    def two_sum(self,nums,target):
+        
+
+def main():
+    nums=[5,7,12,13,16]
+    obj=Solution()
+    res=obj.two_sum(nums,19)
+    print("Result:",res)
+
+if __name__=="__main__":
+    main()
