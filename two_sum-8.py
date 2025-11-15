869f8bcb816b94dcc87b57dd060ab351e1d77361
Ravi <ravikaran90@yahoo.com>
2025-11-15 00:01:23 -0800

    Two Sum

--- two_sum-8.py
+++ two_sum-8.py
@@ -1,6 +1,11 @@
 class Solution:
     def two_sum(self,nums,target):
-        
+        diction={}
+        for i in range(len(nums)):
+            compl=target-nums[i]
+            if compl in diction:
+                return [diction[compl],i]
+            diction[nums[i]]=i
 
 def main():
     nums=[5,7,12,13,16]
