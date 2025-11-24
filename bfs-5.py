595f0bf5ef2a7af506c63721473e59d7489d0ded
Ravi <ravikaran90@yahoo.com>
2025-11-23 19:56:26 -0800

    Breadth First Search

---
+++ bfs-5.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+class Node:
+    def __init__(self,val,left=None,right=None):
+        self.left=left
+        self.val=val
+        self.right=right
+
+class Solution:
+    def bfs(self,root):
+        res=[]
+        q=deque()
+        q.append(root)
+
+        while q:
+            level=[]
+            for i in range(len(q)):
+                node=q.popleft()
+                if node:
+                    level.append(node.val)
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
+            res.append(level)
+        return res
+
+def main():
+    root=Node(500)
+    root.left=Node(300)
+    root.left.left=Node(200)
+    root.left.right=Node(350)
+    root.right=Node(800)
+    root.right.left=Node(750)
+    root.right.right=Node(1000)
+    obj=Solution()
+    res=obj.bfs(root)
+    print("Result:",res)
+
+if __name__=="__main__":
+    main()
