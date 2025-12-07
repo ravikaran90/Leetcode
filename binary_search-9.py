0a2ff9baaa765983ea32176419fc2226b239de04
Ravi <ravikaran90@yahoo.com>
2025-12-06 16:05:44 -0800

    Binary Search

---
+++ binary_search-9.py
@@ -0,0 +1,22 @@
+class Solution:
+    def binary_search(self,nums,target):
+        left=0
+        right=len(nums)
+        while left<=right:
+            mid=(left+right)//2
+            if target==nums[mid]:
+                return mid
+            elif target<nums[mid]:
+                right=mid-1
+            elif target>nums[mid]:
+                left=mid+1
+        return False
+
+def main():
+    nums=[3,6,8,12,15,18,20,23,25,28,31,35,40]
+    obj=Solution()
+    res=obj.binary_search(nums,31)
+    print("Result:",res)
+
+if __name__=="__main__":
+    main()
