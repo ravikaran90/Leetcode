699792af50d1d7f263cbc32a6369483e6830dbf2
Ravi <ravikaran90@yahoo.com>
2025-12-12 00:01:32 -0800

    Subsets

--- subsets-5.py
+++ subsets-5.py
@@ -1,15 +1,16 @@
 from collections import Counter
 
 class Solution:
-    def subsets(self,root):
+    def subsets(self,nums):
         def backtrack(start,current_list):
             result.append(current_list[:])
-            for i in range(start,len(nums):
-                
+            for i in range(start,len(nums)):
+                current_list.append(nums[i])
+                backtrack(i+1,current_list)
+                current_list.pop()
         result=[]
         backtrack(0,[])
         return result
-
 
 def main():
     nums=[5,7,9]
