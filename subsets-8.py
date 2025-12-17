01e22a0ff3648b4ed70c12169cc70d249ec6048e
Ravi <ravikaran90@yahoo.com>
2025-12-16 23:57:58 -0800

    Subsets

---
+++ subsets-8.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+class Solution:
+    def subsets(self,nums):
+        
+                    
+
+def main():
+    nums=[5,23,42]
+    obj=Solution()
+    res=obj.subsets(nums)
+    print("Result:",res)
+
+if __name__=="__main__":
+    main()
