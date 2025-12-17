a4bc1a548ea8a778b9b8f4b07fe9f2d2e4252152
Ravi <ravikaran90@yahoo.com>
2025-12-17 00:02:56 -0800

    Subsets(Updated)

--- subsets-8.py
+++ subsets-8.py
@@ -1,9 +1,16 @@
-from collections import Counter
+import heapq
 
 class Solution:
     def subsets(self,nums):
-        
-                    
+            def backtrack(start,current_list):
+                result.append(current_list[:])
+                for i in range(start,len(nums)):
+                     current_list.append(nums[i])
+                     backtrack(i+1,current_list)
+                     current_list.pop()
+            result=[]
+            backtrack(0,[])
+            return result        
 
 def main():
     nums=[5,23,42]
