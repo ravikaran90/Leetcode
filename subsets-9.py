5991da94196b547ca233cfbc48d36330b3c78c4e
Ravi <ravikaran90@yahoo.com>
2025-12-18 22:59:04 -0800

    Subsets

---
+++ subsets-9.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsets(self,nums):
+        def backtrack(start,current_list):
+            result.append(current_list[:])
+            for i in range(start,len(nums)):
+                current_list.append(nums[i])
+                backtrack(i+1,current_list)
+                current_list.pop()
+        result=[]
+        backtrack(0,[])
+        return result
+
+def main():
+    nums=[42,24,88]
+    obj=Solution()
+    res=obj.subsets(nums)
+    print("Result:",res)
+
+if __name__=="__main__":
+    main()
