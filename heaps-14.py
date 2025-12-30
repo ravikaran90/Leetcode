f1f55c8be0bbe2649c884562f7a2a419809efb08
Ravi <ravikaran90@yahoo.com>
2025-12-30 00:23:12 -0800

    Heaps

--- heaps-14.py
+++ heaps-14.py
@@ -1,15 +1,27 @@
 import heapq
-
+from collections import Counter
 
 class Solution:
-    def heaps(self,):
-        
+    def heaps(self,arr):
+        heapq.heapify(arr)
+        print("Min Heap:",arr)
+        heapq._heapify_max(arr)
+        print("Max Heap:",arr)
+    
+    def topkfrequent(self,arr,k):
+        counts=Counter(arr)
+        heap=[]
+        for key,value in counts.items():
+            heapq.heappush(heap,(key,value))
+            while len(heap)>k:
+                heapq.heappop(heap)
+        return [pair[1] for pair in heap]
 
 def main():
-    nums=[5,9,12]
     arr=[1,1,1,3,3,2,5,5,5,3,4,2]
     obj=Solution()
-    res=obj.heaps(nums)
+    obj.heaps(arr)
+    res=obj.topkfrequent(arr,2)
     print("Result:",res)
 
 if __name__=="__main__":
