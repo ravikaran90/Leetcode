c484ace26f5c1aa27decb1c02dfe2d4011913cd2
Ravi <ravikaran90@yahoo.com>
2025-12-30 00:00:12 -0800

    Subsets

---
+++ heaps-14.py
@@ -0,0 +1,16 @@
+import heapq
+
+
+class Solution:
+    def heaps(self,):
+        
+
+def main():
+    nums=[5,9,12]
+    arr=[1,1,1,3,3,2,5,5,5,3,4,2]
+    obj=Solution()
+    res=obj.heaps(nums)
+    print("Result:",res)
+
+if __name__=="__main__":
+    main()
