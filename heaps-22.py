f3bf10a14d8978b3e7f270332117915b32f1acc8
Ravi <ravikaran90@yahoo.com>
2026-01-10 20:30:24 -0800

    Heaps & Top K Frequent

--- heaps-22.py
+++ heaps-22.py
@@ -1,1 +1,29 @@
+import heapq
+from collections import Counter
+
+class Solution:
+    def heaps(self,nums):
+        heapq.heapify(nums)
+        print("Min Heap:",nums)
+        heapq._heapify_max(nums)
+        print("Max Heap:",nums)
+    def topkfrequent(self,arr,k):
+        counts=Counter(arr)
+        heap=[]
+        for key,value in counts.items():
+            heapq.heappush(heap,(key,value))
+            while len(heap)>k:
+                heapq.heappop(heap)
+        return [pair[1] for pair in heap]
+
+def main():
+    nums=[34,67,32,5,89,32,40,8,6,9]
+    arr=[1,1,1,6,6,3,3,2,2,2,8,12,12]
+    obj=Solution()
+    obj.heaps(nums)
+    res=obj.topkfrequent(arr,3)
+    print("Top K Frequent:",res)
+
+if __name__=="__main__":
+    main()
 
