334c6288e971df2e48527bf01c4f3b64f3c4257c
Ravi <ravikaran90@yahoo.com>
2026-01-10 20:20:30 -0800

    Palindrome

--- palindrome-2.py
+++ palindrome-2.py
@@ -1,1 +1,21 @@
+class Solution:
+    def palindrome(self,string):
+        left=0
+        right=len(string)-1
+        while left<=right:
+            if string[left]!=string[right]:
+                return False
+            else:
+                left+=1
+                right-=1
+        return True
 
+
+def main():
+    string="ravi"
+    obj=Solution()
+    res=obj.palindrome(string)
+    print("Is String Palindrome:",res)
+
+if __name__=="__main__":
+    main()
