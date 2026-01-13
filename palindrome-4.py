81bf4ceb93baf49a18cb04557e31ac95c592d246
Ravi <ravikaran90@yahoo.com>
2026-01-12 23:54:27 -0800

    Palindrome

--- palindrome-4.py
+++ palindrome-4.py
@@ -1,1 +1,15 @@
+class Solution:
+    def palindrome(self,string):
+        left=0
+        right=len(string)-1
+        while left<=right:
+            string[left]
 
+def main():
+    string="kayak"
+    obj=Solution()
+    res=obj.palindrome(string)
+    print("Result:",res)
+
+if __name__=="__main__":
+    main()
