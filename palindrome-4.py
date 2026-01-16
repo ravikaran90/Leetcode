b9a71af3b668d5a21f01b19f4d2c5603410072a0
Ravi <ravikaran90@yahoo.com>
2026-01-15 23:29:51 -0800

    Palindrome

--- palindrome-4.py
+++ palindrome-4.py
@@ -3,13 +3,17 @@ class Solution:
         left=0
         right=len(string)-1
         while left<=right:
-            string[left]
+            if string[left]!=string[right]:
+                return False
+            left+=1
+            right-=1
+        return True
 
 def main():
-    string="kayak"
+    string="123321"
     obj=Solution()
     res=obj.palindrome(string)
-    print("Result:",res)
+    print("Is String Palindrome:",res)
 
 if __name__=="__main__":
     main()
