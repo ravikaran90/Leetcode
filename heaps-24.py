1402eec077a3cfc70e372105e3e2ad98b1151abf
Ravi <ravikaran90@yahoo.com>
2026-01-19 00:04:24 -0800

    Heaps & Top K Frequent

--- heaps-24.py
+++ heaps-24.py
@@ -1,26 +1,29 @@
 import heapq
-from collections import deque
-
-class Node:
-    def __init__(self,val,left=None,right=None):
-        self.left=left
-        self.val=val
-        self.right=right
+from collections import Counter
 
 class Solution:
-    def heaps():
+    def heaps(self,nums):
+        heapq.heapify(nums)
+        print("Min Heap:",nums)
+        heapq._heapify_max(nums)
+        print("Max Heap:",nums)
+
+    def topkfrequent(self,arr,k):
+        counts=Counter(arr)
+        heap=[]
+        for key,value in counts.items():
+            heapq.heappush(heap,(key,value))
+            while len(heap)>k:
+                heapq.heappop(heap)
+        return [pair[1] for pair in heap]
 
 def main():
-    root=Node(100)
-    root.left=Node(125)
-    root.right=Node(250)
-    root.left.left=Node(500)
-    root.left.right=Node(1000)
-    root.right.left=Node(2000)
-    root.right.right=Node(4000)
+    nums=[23,34,12,25,13,18,4,89,76,3,90,11]
+    arr=[1,1,1,1,4,4,4,3,3,2,2,8,8,8]
     obj=Solution()
-    res=obj.bfs(root)
-    print("Result:",res)
+    obj.heaps(nums)
+    res=obj.topkfrequent(arr,3)
+    print("Top K Frequent:",res)
 
 if __name__=="__main__":
     main()
