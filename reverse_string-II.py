c4041a58bf980457903d74aaa83ea292842354a7
Ravi <ravikaran90@yahoo.com>
2026-01-21 22:55:17 -0800

    Reverse String

---
+++ reverse_string-II.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverse_string(self,string,k):
+        for i in range(len(string)):
+            
+
+def main():
+    string="abcdefg"
+    k=2
+    obj=Solution()
+    res=obj.reverse_string(string,k)
+    print("Result:",res)
+
+if __name__=="__main__":
+    main()
+
+
