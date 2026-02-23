4b6ce2198fb487eea0ef5f94b2bf73bffcea6f42
Ravi <ravikaran90@yahoo.com>
2026-02-22 20:03:34 -0800

    Recusion

--- recursion-4.py
+++ recursion-4.py
@@ -1,1 +1,12 @@
+def recursion(num):
+    if num<5:
+        return
+    else:
+        print(num)
+        recursion(num-1)
 
+def main():
+    recursion(20)
+
+if __name__=="__main__":
+    main()
