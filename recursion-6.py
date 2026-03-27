c07143fa58cc583f2343aa93f9f13b0a43165c81
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-03-26 23:03:06 -0700

    Add recursion function to handle number printing
    Implement a recursive function to print numbers and return values based on conditions.

--- recursion-6.py
+++ recursion-6.py
@@ -1,1 +1,13 @@
+def recursion(n):
+  if n<5:
+    return n
+  else:
+    print(n)
+    return recursion(n-1)+recursion(n-2)
 
+def main():
+  n=10
+  res=recursion(n)
+
+if _name_==__main__:
+  main()
