871d543630313cecec0ac668b26312848d080bc0
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-03-31 19:05:06 -0700

    Recursion
    Add a recursive function to compute values based on n.

--- recursion-15.py
+++ recursion-15.py
@@ -1,1 +1,13 @@
+def recursion(n):
+  if n<0:
+    return
+  else:
+    recursion(n-1)+recursion(n-2)
 
+def main():
+  n=10
+  res=recursion(n)
+  print("Result:",res)
+
+if __name__==__main__:
+  main()
