d99b7e3525aaff1974f8a93a2c6134cf84d87d74
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-04-02 19:59:47 -0700

    Heaps

--- heaps-44.py
+++ heaps-44.py
@@ -1,1 +1,17 @@
+import heaps
 
+class Solution:
+  def heaps(self,n):
+    heaps.heapify(n)
+    print("Min Heap:",n)
+    heaps._max_heapify(n)
+    print("Max Heap:",n)
+
+def main():
+  n=[60,63,55,52,89,4,56,90]
+  obj=Solution()
+  res=obj.heaps(n)
+
+if __name__==__main__:
+  main()
+    
