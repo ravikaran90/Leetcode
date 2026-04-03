dbb223f0b94cd03d0c144740866dc37d0c9a510c
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-04-02 20:14:23 -0700

    Subsets
    Implement a subset generation method using backtracking.

--- subsets-41.py
+++ subsets-41.py
@@ -1,1 +1,17 @@
+class Solution:
+  def subsets(self,n):
+    def backtrack(start,):
+        for i in range(len(n)):
+          backtrack(start,current_list)
+    
+    res=backtrack(0,[])
+    print("Result:",res)
 
+def main():
+  n=[5,12,15]
+  obj=Solution()
+  res=obj.subsets(n)
+  print("Subsets:",res)
+
+if __name__==__main__:
+  main()
