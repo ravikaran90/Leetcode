06c2ad61d7c755d1dd2744b95c8bc8c0f8d7e85f
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-04-03 20:29:08 -0700

    Heaps
    Added a Solution class with a heaps method to create and print min and max heaps.

--- heaps-47.py
+++ heaps-47.py
@@ -1,1 +1,16 @@
+import heaps
 
+class Solution:
+  def heaps(self,n):
+    heaps.heapify(n)
+    print("Min Heap:",n)
+    heaps._heapify_max(n)
+    print("Max Heap:",n)
+
+def main():
+  n=[56,58,88,43,41,34,2,4,8,90,32]
+  obj=Solution()
+  obj.heaps(n)
+
+if __name__==__main__:
+  main()
