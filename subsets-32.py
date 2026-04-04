b26569ec0d3245cc2e7aa076181699a40f137616
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-04-03 20:36:50 -0700

    Subsets
    Added a class Solution with a method to generate subsets using backtracking. Implemented a main function to demonstrate usage.

--- subsets-32.py
+++ subsets-32.py
@@ -1,1 +1,16 @@
+import heaps
 
+class Solution:
+  def subsets(self,n):
+    def backtrack(n,current_list):
+      for i in range(n):
+        current_list.append(n)
+
+def main():
+  n=[56,58,32]
+  obj=Solution()
+  res=obj.subsets(n)
+  print("Subsets:",res)
+
+if __name__==__main__:
+  main()
