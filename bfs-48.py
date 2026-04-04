caac6f6c05bbf58f208cf422f2017d04bbc2c2f7
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-04-03 20:34:21 -0700

    BFS
    Fix BFS implementation and correct variable usage.

--- bfs-48.py
+++ bfs-48.py
@@ -1,1 +1,31 @@
+from collections import deque
 
+class Node:
+  def __init__(self,val,left=None,right=None):
+    self.left=left
+    self.val=val
+    self.right=right
+
+class Solution:
+  def bfs(self,n):
+    res=[]
+    q=deque([root])
+    while q:
+      node=node.deque()
+      if node.left:
+        q.apppend(node.left)
+      if node.right:
+        q.append(node.right)
+      level.append(q)
+    res.append(level)
+
+def main():
+  root=Node(75)
+  root.left=Node(100)
+  root.right=Node(125)
+  obj=Solution()
+  res=obj.bfs(root)
+  print("Result:",res)
+
+if __name__==__main__:
+  main()
