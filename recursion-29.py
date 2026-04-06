49ac46d8d3a7ea6e2687d3f6320beb996b91e80e
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-04-05 23:53:11 -0700

    Recursion
    Fix recursion function definition and call syntax.

--- recursion-29.py
+++ recursion-29.py
@@ -1,1 +1,14 @@
+def recursion:
+    if n<1:
+      return
+    else:
+      recursion(n-1)+recursion(n-2)
+    
+def main():
+  n=5
+  obj=Solution()
+  res=obj.recursion(n)
+  print("Result:",res)
 
+if __name__==__main__:
+  main()
