12728a80081da235561575554d9209a9c09246f8
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-04-05 23:47:03 -0700

    BFS
    Implement BFS traversal for a binary tree using a queue.

--- bfs-45.py
+++ bfs-45.py
@@ -1,1 +1,30 @@
+from collections import deque
 
+class Node:
+  def __init__(self,val,left=None,right=None):
+    self.val=val
+    self.left=left
+    self.right=right
+
+class Solution:
+  def bfs(self,root):
+    res=[]
+    q=deque([root])
+    while q:
+      node=q.popleft()
+      while node:
+        if node.left:
+          q.append(node.left)
+        if node.right:
+          q.append(node.right)
+      level.append(q)
+    res.append(level)
+    
+def main():
+  root=Node(100)
+  obj=Solution()
+  res=obj.bfs(root)
+  print("Result:",res)
+
+if __name__==__main__:
+  main()
