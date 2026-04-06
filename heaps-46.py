a89d21642d761522d10fa52a0c6b456638fd6b68
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-04-05 23:50:03 -0700

    Heaps
    Added a Solution class to create min and max heaps from a list of numbers.

--- heaps-46.py
+++ heaps-46.py
@@ -1,1 +1,18 @@
+import heaps
+
+class Solution:
+  def heaps(self,n):
+    heaps.heapify(n)
+    print("Min Heap:",n)
+    heaps._heapify_.max(n)
+    print("Max Heap:",n)
+    
+def main():
+  n=[56,67,89,78,43,46,72,5,90,21]
+  obj=Solution()
+  res=obj.heaps(n)
+  print("Result:",res)
+
+if __name__==__main__:
+  main()
 
