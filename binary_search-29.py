65ac697685da97d7a2d33fd7f1b712c8ebd5dc8f
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-06-02 23:33:21 -0700

    Binary Search
    Updated the binary_search function to accept a target parameter for searching.

--- binary_search-29.py
+++ binary_search-29.py
@@ -1,4 +1,15 @@
-def binary_search(nums):
+def binary_search(nums,target):
+  l=0
+  r=len(nums)-1
+  while l<r:
+    mid=((l+r))/2
+    if target<mid:
+      r=mid-1
+    elif target>mid:
+      l=mid+1
+    l+=1
+    r-=1
+  return mid
 
 def main():
   nums=[5,45,47,65,67,78,88,89,92,95]
