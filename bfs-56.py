f139e0bf61f61807c2f278154421ee8eb3e8263a
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-06-18 23:25:08 -0700

    BFS

--- bfs-56.py
+++ bfs-56.py
@@ -9,6 +9,12 @@ def bfs(root):
   
 def main():
   root=Node(67)
+  root.left=Node(100)
+  root.right=Node(50)
+  root.right.right=Node(150)
+  root.right.left=Node(55)
+  root.left.left=Node(89)
+  root.left.right=Node(120)
   res=bfs(root)
   print("Result:",res)
 
