b8c30deea2b59a07003b3c179880afe28071eb27
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-06-24 23:35:21 -0700

    BFS

--- bfs-62.py
+++ bfs-62.py
@@ -1,4 +1,5 @@
-  def __init__(self,val,left=None,right=None):
-    self.val=val
-    self.left=left
-    self.right=right
+class Node:
+    def __init__(self,val,left=None,right=None):
+        self.val=val
+        self.left=left
+        self.right=right
