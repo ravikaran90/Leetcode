d09c377f4a1ba6df5b53457b16a533245bd916c2
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-06-24 23:32:03 -0700

    BFS

--- bfs-61.py
+++ bfs-61.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Node:
   def __init__(self,val,left=None,right=None):
     self.val=val
@@ -6,6 +8,16 @@ class Node:
 
 def bfs(root):
   res=[]
+  q=deque([root])
+  while q:
+    level=[]
+    node=q.popleft()
+    if node.left:
+      q.append(node.left)
+    if node.right:
+      q.append(node.right)
+    res.append(level)
+  return res
 
 def main():
   res=bfs(root)
