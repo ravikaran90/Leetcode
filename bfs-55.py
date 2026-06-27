c00655632aa834cb91ef0b0d4325aec68aa06689
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-06-26 23:39:27 -0700

    BFS
    Fix BFS implementation to correctly traverse tree levels and append nodes to results.

--- bfs-55.py
+++ bfs-55.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 class Node:
   def __init__(self,val,left=None,right=None):
     self.val=val
@@ -5,6 +7,18 @@ class Node:
     self.right=right
 
 def bfs(root):
+  res=[]
+  q=deque([root])
+  while q:
+    level=[]
+    node=q.popleft()
+    for i in range(q):
+      if node.left:
+        q.append(node.left)
+      if node.right:
+        q.append(node.right)
+      level.append(q)
+  res.append(level)
 
 def main():
   res=bfs(root)
