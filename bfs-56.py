486ea3a556e4825a492f4150d43e6464f991f9e8
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-06-27 23:23:11 -0700

    BFS

--- bfs-56.py
+++ bfs-56.py
@@ -9,14 +9,15 @@ def bfs(root):
   q=deque([root])
   while q:
     level=[]
-    node=q.popleft()
-    for i in range(q):
+    for i in range(len(q)):
+      node=q.popleft()
       if node.left:
         q.append(node.left)
       if node.right:
         q.append(node.right)
       level.append(q)
   res.append(level)
+  return res
   
   
 def main():
