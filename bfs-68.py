ec7c818523400fadb7bbc165b9006e1849ffd760
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-07-02 23:50:52 -0700

    BFS

--- bfs-68.py
+++ bfs-68.py
@@ -5,8 +5,20 @@ class Node:
     self.right=right
 
 def bfs():
+  res=[]
+  q=deque([root])
+  while q:
+    level=[]
+    node=q.popleft()
+    for i in range(len(q)):
+      level.append(node.val)
+      if node.left:
+        q.append(node.left)
+      if node.right:
+        q.append(node.right)
+    res.append(level)
+  return res
   
-
 def main():
   res=bfs(root)
 
