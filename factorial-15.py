826c97b3fe75c6a2b79449f78422bca53fa38005
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-07-21 23:44:26 -0700

    Factorial

--- factorial-15.py
+++ factorial-15.py
@@ -1,1 +1,13 @@
 def factorial(n)
+  if n<=0:
+    return 0
+  else:
+    b=factorial(n)*factorial(n-1)
+
+def main():
+  n=5
+  res=factorial(n)
+  print("Result:",res)
+
+if __name__==__main__:
+  main()
