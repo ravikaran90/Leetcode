1324b1b865486b30cb0a815197e6a8b3210c13e9
Ravi Karan Anand <ravikaran90@yahoo.com>
2026-07-30 23:49:46 -0700

    Binary Search

--- binary_search-19.py
+++ binary_search-19.py
@@ -1,8 +1,18 @@
-def binary_search(n):
+def binary_search(n,target):
+  l=0
+  r=len(n)-1
+  while l<r:
+    mid=(l+r)//2
+    if target>mid:
+      l=mid+1
+    elif target<mid:
+      r=mid-1
+    l+=1
+    r-=1
 
 def main():
   n=[6,8,78,90,35,32]
-  res= binary_search(n)
+  res= binary_search(n,78)
   print("Result:",res)
 
 if __name__==__main__:
