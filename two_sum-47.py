eb707b1f6e456e77b031843223bd3454f1b1a1ef
Ravi <ravi@Ravis-MacBook-Pro.local>
2026-09-20 14:00:53 -0700

    Two Sum

--- two_sum-47.py
+++ two_sum-47.py
@@ -1,1 +1,27 @@
+class Solution:
+    def twosum(self, arr, target):
+        sum=0
+        for i in range(len(arr)):
+            left=0
+            right=len(arr)-1
+            sum=arr[left]+arr[right]
+            if sum==target:
+                return [left,right]
+            elif sum<target:
+                left+=1
+            elif sum>target:
+                right+=1
+        return None
+
+def main():
+    arr=[3,5,8,10,13,16,19,21,25]
+    obj=Solution()
+    res=obj.twosum(arr,32)
+    print("Result:",res)
+
+if __name__=="__main__":
+    main()
+
+
+
 
